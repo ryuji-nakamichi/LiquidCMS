fd73b3a1fedb438b186f65f64b5c5cc4a8c1b947
ryuji-nakamichi <ryuji6239@gmail.com>
2023-04-17 13:49:18 +0900

    【開発中】 ■ルーティング - コメントアウトを削除

--- web/index.php
+++ web/index.php
@@ -2,16 +2,7 @@
 
 require_once('lib/Route.php');
 require_once('config/route.php');
-
 use Liqsyst\Utility\RouteClass as Route;
 
-// $path_info = (isset($_SERVER["PATH_INFO"]) && !is_null($_SERVER["PATH_INFO"])) ? $_SERVER["PATH_INFO"] : '/';
 $route = new Route($routMap);
 $route->run();
-// require_once('controllers/' . $routeMap['info']['controller'] . '.' . 'php');
-
-// echo $route_str;
-
-// echo '<pre>';
-// print_r($_SERVER);
-// echo '</pre>';
